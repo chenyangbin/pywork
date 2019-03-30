d5483dd4e97829df6af6944ea8ed80a0a0c4a497
chenyangbin <342529137@qq.com>
2019-03-30 16:12:08 +0800

    commit note 2

--- day11面向对象案例实现/01案例实现.py
+++ day11面向对象案例实现/01案例实现.py
@@ -10,20 +10,70 @@ class Dog:
         self.age = age
 
     def eat(self):  #小狗吃
-        print("名字是{},年龄{}岁的小狗在吃饭{}".format(self.name, self.age))
+        #print("名字是{},年龄{}岁的小狗在吃饭{}".format(self.name, self.age))
+        print("%s在吃饭"%self)
 
     def play(self):  #小狗玩儿方法
-        print("名字是{},年龄{}岁的小狗在玩儿".format(self.name,self.age))    
+        #print("名字是{},年龄{}岁的小狗在玩儿".format(self.name,self.age))    
+        print("%s在玩儿"%self)
+
 
     def sleep(self):  #小狗睡方法
-        print("名字是{},年龄{}岁的小狗在睡觉".format(self.name,self.age))
+        #print("名字是{},年龄{}岁的小狗在睡觉".format(self.name,self.age))
+        print("%s在睡觉"%self)
+
 
     def __str__(self):
-        
+        return "名字是{},年龄{}岁的小狗".format(self.name, self.age)
+    
+
+
+class Person:
+    def __init__(self,name,pet, age = 1):
+
+        self.name = name 
+        self.age = age
+        self.pet = pet
+
+    def eat(self):  #人吃
+        #print("名字是{},年龄{}岁的主人在吃饭{}".format(self.name, self.age))
+        print("%s在吃饭"%self)
+
+    def play(self):  #人玩儿方法
+        #print("名字是{},年龄{}岁的主人在玩儿".format(self.name,self.age))    
+        print("%s在吃饭"%self)
+
+
+    def sleep(self):  #人睡方法
+        #print("名字是{},年龄{}岁的主人在睡觉".format(self.name,self.age))
+        print("%s在吃饭"%self)
+
+    def yangpet(self):
+        for pet in self.pet:
+            pet.eat()
+            pet.play()
+            pet.sleep()
+
+    def __str__(self):
+        return "名字是{},年龄{}岁的小狗".format(self.name, self.age)
+
+    def make_pet_work(self):
+        for pet in self.pet:
+            if isinstance(pet, Dog)
+            pet.play()
+            elif expression:
+                pass
 
 
 
-dog_hei = Dog("小黑", 12)
+
+    
+dog_hei = Dog("小黑", 12) #实例化宠物小黑
+
+person_qing = Person("qing", [dog_hei],20 )
+person_qing.yangpet()
+person_qing.make_pet_work()
+
 print(dog_hei.eat())
 
 print(dog_hei.__dict__)
